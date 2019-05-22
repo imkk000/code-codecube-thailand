df4178e7bf72522fa1eb8575c15283c14e3e3195
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-05-22 19:27:46 +0700

    [Added]: source code of problem name 'AxB Problem' (1)

---
+++ p1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  unsigned long long number1, number2;
+  cin >> number1 >> number2;
+  cout << number1 * number2 << endl;
+  return 0;
+}
